518397494b0f73c4f6fd90f4447b894da2dcb360
shoIwasaki <>
2021-04-26 00:53:07 +0900

    新規作成

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="title">
+      <img class="bone-img bone" src="../img/骨のフリーアイコン.png" alt="骨" />
+      <h1 class="bone">Pet Items</h1>
+    </div>
+    <Side class="side" />
+    <Items class="items" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Side from "./components/side.vue";
+import Items from "./components/items.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Side,
+    Items,
+  },
+};
 </script>
 
 <style>
@@ -19,8 +26,27 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.container {
+  display: grid;
+  grid-template-columns: 220px 1fr;
+  grid-template-rows: 50px 1fr 50px;
+}
+.title {
+  margin: 0;
+  font-size: 10px;
+  grid-column-start: 1;
+  grid-column-end: 3;
+  grid-row-start: 1;
+  grid-row-end: auto;
+  background-color: aqua;
+}
+.bone-img {
+  padding: 0 5px;
+  width: 13px;
+  height: 13px;
+}
+.bone {
+  display: inline-block;
 }
 </style>
