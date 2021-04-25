1fbfb028b85a8594003f1b4b6f73d0f30eb500db
shoIwasaki <>
2021-04-26 00:46:31 +0900

    新規作成

---
+++ src/components/items.vue
@@ -0,0 +1,36 @@
+<template>
+  <main id="main">
+    <div id="items" v-for="item in items" :key="item.id">
+      <img :src="item.path" alt="画像" />
+      <p>¥:{{ item.value }}</p>
+      <p>種類:{{ item.type }}</p>
+      <button><a :href="item.link">購入先</a></button>
+    </div>
+  </main>
+</template>
+
+<script>
+import items from "../assets/items.json";
+export default {
+  name: "side",
+  data() {
+    return {
+      items: items,
+    };
+  },
+};
+</script>
+
+<style scoped>
+#main {
+  grid-column-start: 2;
+  grid-column-start: auto;
+  grid-row-start: 2;
+  grid-row-end: auto;
+  background-color: teal;
+}
+#items {
+  display: inline-block;
+  padding-left: 20px;
+}
+</style>
---
+++ src/components/side.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <aside class="side">
+      <h2>TYPE</h2>
+      <ul>
+        <li class="type">リード</li>
+        <li class="type">ペット服</li>
+        <li class="type">おもちゃ</li>
+        <li class="type">おやつ</li>
+      </ul>
+    </aside>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "side",
+};
+</script>
+
+<style scoped>
+.side {
+  box-sizing: border-box;
+  padding: 0 10px;
+  background-color: #fff;
+  grid-column-start: 1;
+  grid-column-end: 2;
+  grid-row-start: 2;
+  grid-row-end: 3;
+  background-color: blueviolet;
+}
+.type {
+  list-style: none;
+  padding: 10px;
+
+  margin: 10px 0;
+}
+ul {
+  margin: 0;
+  padding: 0;
+}
+</style>
