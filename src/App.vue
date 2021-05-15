ec062078f1c005f10db6a0de0f17a7c9483577c4
shoIwasaki <>
2021-05-16 01:35:25 +0900

    機能追加

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,57 @@
     <aside class="side sticky">
       <h2>TYPE</h2>
       <ul>
-        <li @click="select('アイテム')" class="type">アイテム</li>
-        <li @click="select('フード')" class="type">フード</li>
-        <li @click="select('ペット服')" class="type">ペット服</li>
+        <!-- サイドバーのアイテムを選択すると背景色がグレーになる
+        カーソルを載せると背景色がライトグレーになる -->
+        <li
+          @click="select('アイテム'), switchColor1()"
+          class="type"
+          :class="{
+            gray: sideItem,
+            white: !sideItem,
+            lightGray: hoverItem && !sideItem,
+          }"
+          @mouseover="mouseover1()"
+          @mouseleave="mouseleave1()"
+        >
+          アイテム
+        </li>
+        <!-- サイドバーのフードを選択すると背景色がグレーになる
+        カーソルを載せると背景色がライトグレーになる -->
+        <li
+          @click="select('フード'), switchColor2()"
+          class="type"
+          :class="{
+            gray: sideFood,
+            white: !sideFood,
+            lightGray: hoverFood && !sideFood,
+          }"
+          @mouseover="mouseover2()"
+          @mouseleave="mouseleave2()"
+        >
+          フード
+        </li>
+        <!-- サイドバーのペット服を選択すると背景色がグレーになる
+        カーソルを載せると背景色がライトグレーになる -->
+        <li
+          @click="select('ペット服'), switchColor3()"
+          class="type"
+          :class="{
+            gray: sideFastion,
+            white: !sideFastion,
+            lightGray: hoverFastion && !sideFastion,
+          }"
+          @mouseover="mouseover3()"
+          @mouseleave="mouseleave3()"
+        >
+          ペット服
+        </li>
       </ul>
     </aside>
-    <!-- <Side class="side" @click="select('アイテム')" /> -->
-    <Items class="items" :type="type" />
+    <Items
+      :class="{ fedeIn: classSwitch, fedeIn2: !classSwitch }"
+      :type="type"
+    />
   </div>
 </template>
 
@@ -31,11 +75,69 @@ export default {
   data() {
     return {
       type: "",
+      sideItem: false,
+      sideFood: false,
+      sideFastion: false,
+      hoverItem: false,
+      hoverFood: false,
+      hoverFastion: false,
+      classSwitch: false,
     };
   },
   methods: {
     select(e) {
       this.type = e;
+    },
+    switchColor1() {
+      //アイテム
+      this.sideItem = true;
+      this.sideFood = false;
+      this.sideFastion = false;
+      this.classSwitch = !this.classSwitch;
+    },
+    switchColor2() {
+      //フード
+      this.sideItem = false;
+      this.sideFood = true;
+      this.sideFastion = false;
+      this.classSwitch = !this.classSwitch;
+    },
+    switchColor3() {
+      //ペット服
+      this.sideItem = false;
+      this.sideFood = false;
+      this.sideFastion = true;
+      this.classSwitch = !this.classSwitch;
+    },
+    mouseover1() {
+      //アイテム
+      this.hoverItem = true;
+      this.hoverFood = false;
+      this.hoverFastion = false;
+    },
+    mouseover2() {
+      //フード
+      this.hoverItem = false;
+      this.hoverFood = true;
+      this.hoverFastion = false;
+    },
+    mouseover3() {
+      //ペット服
+      this.hoverItem = false;
+      this.hoverFood = false;
+      this.hoverFastion = true;
+    },
+    mouseleave1() {
+      //アイテム
+      this.hoverItem = false;
+    },
+    mouseleave2() {
+      //フード
+      this.hoverFood = false;
+    },
+    mouseleave3() {
+      //ペット服
+      this.hoverFastion = false;
     },
   },
 };
@@ -49,7 +151,7 @@ export default {
 }
 .container {
   display: grid;
-  grid-template-columns: 220px 1fr;
+  grid-template-columns: 180px 1fr;
   grid-template-rows: 50px 1fr 50px;
 }
 .title {
@@ -90,11 +192,43 @@ export default {
 .type {
   list-style: none;
   padding: 10px;
-
   margin: 10px 0;
+  cursor: pointer;
 }
 ul {
   margin: 0;
   padding: 0;
+}
+.gray {
+  background-color: #cccccc;
+  transition-duration: 0.2s;
+}
+.white {
+  background-color: white;
+}
+.lightGray {
+  background-color: #eeeeee;
+}
+.fedeIn {
+  animation: 0.5s fadeIn;
+}
+.fedeIn2 {
+  animation: 0.5s fadeIn2;
+}
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+@keyframes fadeIn2 {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
 }
 </style>
--- src/components/items.vue
+++ src/components/items.vue
@@ -3,14 +3,40 @@
     <div id="items" v-for="item in select" :key="item.id">
       <img :src="item.path" alt="画像" />
       <p>¥:{{ item.value }}</p>
-      <p>種類:{{ item.type }}</p>
-      <button>
+      <!-- <p>種類:{{ item.type }}</p> -->
+      <button
+        class="btn"
+        onclick="return confirm('外部のページへ移動します。よろしいですか？');"
+      >
         <a :href="item.link" target="_blank" rel="noopener noreferrer"
           >購入先</a
         >
       </button>
     </div>
   </main>
+  <!-- <img src="../assets/img/アイテム1.jpeg" alt="" />
+  <img src="../assets/img/アイテム2.jpeg" alt="" />
+
+  <img src="../assets/img/アイテム3.jpeg" alt="" />
+  <img src="../assets/img/アイテム4.jpeg" alt="" />
+  <img src="../assets/img/アイテム5.jpeg" alt="" />
+  <img src="../assets/img/アイテム6.jpeg" alt="" />
+  <img src="../assets/img/アイテム7.jpeg" alt="" />
+  <img src="../assets/img/アイテム8.jpeg" alt="" />
+  <img src="../assets/img/アイテム9.jpeg" alt="" />
+  <img src="../assets/img/フード1.jpeg" alt="" />
+  <img src="../assets/img/フード2.jpeg" alt="" />
+  <img src="../assets/img/フード3.jpeg" alt="" />
+  <img src="../assets/img/フード4.jpeg" alt="" />
+  <img src="../assets/img/服1.jpeg" alt="" />
+  <img src="../assets/img/服2.jpeg" alt="" />
+  <img src="../assets/img/服3.jpeg" alt="" />
+  <img src="../assets/img/服4.jpeg" alt="" />
+  <img src="../assets/img/服5.jpeg" alt="" />
+  <img src="../assets/img/服6.jpeg" alt="" />
+  <img src="../assets/img/服7.jpeg" alt="" />
+  <img src="../assets/img/服8.jpeg" alt="" />
+  <img src="../assets/img/服9.jpeg" alt="" /> -->
 </template>
 
 <script>
@@ -24,16 +50,18 @@ export default {
     return {
       items: items,
       selectItems: items,
-      sortKey: "",
-      mess: "",
     };
   },
   computed: {
     //親コンポーネントでタイプを指定して表示内容を変更する
     select() {
-      let tests = this.selectItems;
-      let testlist = tests.filter((x) => x.type == this.type);
-      return testlist;
+      if (this.type == "") {
+        return items;
+      } else {
+        let tests = this.selectItems;
+        let testlist = tests.filter((x) => x.type == this.type); //ここのtypeはpropsで指定している
+        return testlist;
+      }
     },
   },
 };
@@ -45,12 +73,16 @@ export default {
   grid-column-start: auto;
   grid-row-start: 2;
   grid-row-end: auto;
-  background-color: #fffaf0;
+  background-color: #f8f8f8;
 }
 #items {
   display: inline-block;
   padding-top: 20px;
   padding-left: 20px;
+  padding: 20px 0 20px 20px;
+}
+#items p {
+  text-align: right;
 }
 #items img {
   width: 250px;
